0061688051ec0f27abf1b76546ed4b150a6ccad2
binhhotat <106874913+binhhotat@users.noreply.github.com>
2022-06-04 21:05:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
     introTitle: 'helu'
     desc: 'cj ông trả lời hoặc thoát ra, tức là cj thích đó nha'
-    btnYes: không
+    btnYes: 'không'
     btnNo: 'coá'
     question:'cj cs yêu bắc không??
-    reasonPlaceholder: 'Lý do',
+    reasonPlaceholder: 'tại vì ',
     btnReply: 'Gởi !',
     reply: 'Tại vì cj hok yêu bắc '
